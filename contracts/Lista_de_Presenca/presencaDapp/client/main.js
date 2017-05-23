f333ecc83fa6434cc2c56e8b79a279124cb54413
Flávio Codeço Coelho <fccoelho@gmail.com>
2017-05-23 16:56:47 -0300

    updated the meteor Dapp

--- contracts/Lista_de_Presenca/presencaDapp/client/main.js
+++ contracts/Lista_de_Presenca/presencaDapp/client/main.js
@@ -3,18 +3,22 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.presenca.onCreated(function presencaOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  EthBlocks.init();
 });
 
-Template.hello.helpers({
+Template.presenca.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  currentBlock() {
+      return EthBlocks.latest.number;
+  }
 });
 
-Template.hello.events({
+Template.presenca.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
