0974cb63e509b6f11177e4600813188f212c3bb5
Flavio C. Coelho <fccoelho@gmail.com>
2017-07-16 07:57:24 -0300

    Atualizei contrato

--- contracts/Lista_de_Presenca/Presenca.sol
+++ contracts/Lista_de_Presenca/Presenca.sol
@@ -1,1 +1,28 @@
- 
+pragma solidity ^0.4.0;
+
+contract Presenca{
+    struct Aluno{
+        bool matriculado;
+        uint presencas;
+    }
+
+    address professor;
+    mapping(address => Aluno) turma;
+
+    function Presenca(){
+        // Professor cria o contrato
+        professor = msg.sender;
+    }
+
+    function registraAluno(address aluno){
+        if (msg.sender != professor) return;
+        turma[aluno].matriculado = true;
+    }
+
+    function assinaPresenca(){
+        // Aluno assina a lista de presenca
+        if (msg.sender == professor || turma[msg.sender].matriculado == false) return;
+        turma[msg.sender].presencas += 1;
+    }
+
+}
