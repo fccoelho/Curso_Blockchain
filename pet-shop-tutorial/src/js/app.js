8c9d96928595b996a650985423870053e1342a6c
Flávio Codeço Coelho <fccoelho@gmail.com>
2021-10-08 11:21:54 -0300

    updated tutorial

--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -24,17 +24,42 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    // Modern dapp browsers...
+if (window.ethereum) {
+  App.web3Provider = window.ethereum;
+  try {
+    // Request account access
+    await window.ethereum.enable();
+  } catch (error) {
+    // User denied account access...
+    console.error("User denied account access")
+  }
+}
+// Legacy dapp browsers...
+else if (window.web3) {
+  App.web3Provider = window.web3.currentProvider;
+}
+// If no injected web3 instance is detected, fall back to Ganache
+else {
+  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+}
+web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data) {
+  // Get the necessary contract artifact file and instantiate it with @truffle/contract
+  var AdoptionArtifact = data;
+  App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+  // Set the provider for our contract
+  App.contracts.Adoption.setProvider(App.web3Provider);
+
+  // Use our contract to retrieve and mark the adopted pets
+  return App.markAdopted();
+});
 
     return App.bindEvents();
   },
@@ -44,9 +69,21 @@ App = {
   },
 
   markAdopted: function() {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
