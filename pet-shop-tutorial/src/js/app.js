cb20de04015319500dc3b0e4c34fdf7313ea05fb
Flávio Codeço Coelho <fccoelho@gmail.com>
2021-10-09 03:02:58 -0300

    updated app

--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -91,9 +91,26 @@ web3 = new Web3(App.web3Provider);
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
